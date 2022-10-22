2fd4e783362ae8e807a0c05cd0ae2afcaf50850f
Shishir Mathur <shishirmns718@gmail.com>
2022-10-22 21:46:58 +0530

    initial project structure

---
+++ android/app/src/main/kotlin/com/example/assgn_social_cawstudios/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assgn_social_cawstudios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'cs_main.dart';
+
+void main() {
+  runApp(const CSApp());
+}
